c8041f5d6b5ae4a6791f10ce517a112a236b6998
MarkHW <lolka-pop@rambler.ru>
2023-05-06 15:56:49 +0300

    feat: 01-read-file done

---
+++ 01-read-file/index.js
@@ -0,0 +1,7 @@
+const fs = require('fs');
+const path = require('path');
+
+const filePath = path.join(__dirname, 'message.txt');
+const rs = fs.createReadStream(filePath);
+
+rs.pipe(process.stdout);
